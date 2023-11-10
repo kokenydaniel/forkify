ff39a80339d10afdbb64592fe1ebcfe611c92737
Daniel Kokeny <129154129+kokenydaniel@users.noreply.github.com>
2023-11-10 10:50:00 +0100

    Update scss and views

---
+++ config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const SM_RES_PER_PAGE = 5;
+export const KEY = '084d8ec6-e52d-416f-83f2-797296e1cae3';
+export const MODAL_CLOSE_SEC = 2.5;
